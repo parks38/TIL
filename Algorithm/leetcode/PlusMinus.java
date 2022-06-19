e844bc122129b820a29736e0dfe8462f7a79b3b6
parks38 <parks38@uw.edu>
2022-06-19 21:19:00 +0900

    feat: algorithm question added

---
+++ Algorithm/leetcode/PlusMinus.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        int[] answer = new int[3];
+        arr.stream().forEach(i -> {
+            if (i > 0)
+                answer[0]++;
+            else if (i < 0)
+                answer[1]++;
+            else
+                answer[2]++;
+        });
+
+        for (int i = 0; i < answer.length; i++) {
+            double total = (double) answer[i] / arr.size();
+            System.out.println(String.format("%.6f", total));
+        }
+    }
+
+}
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
