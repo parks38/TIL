024de099a6caf343c1592ae701c762daf4539b2b
Sunny Park <parks38@uw.edu>
2022-07-13 21:15:36 +0900

    Create 로또.java

---
+++ Algorithm/leetcode/로또.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        
+        List<Integer> winList = Arrays.stream(win_nums).boxed().collect(Collectors.toList());
+     
+        int count = 0;
+        int zero = 0;
+        
+        for (int i = 0; i < lottos.length; i++) {
+            
+            if (lottos[i] == 0) zero++;
+            if (winList.contains(lottos[i])) count++;
+        }
+        
+        answer[0] = 6 - (count+zero) + 1;
+        answer[1] = 6 - (count) + 1;
+        if (zero == 6) {
+            answer[1] = 6;
+        }
+        
+        return answer;
+    }
+}
